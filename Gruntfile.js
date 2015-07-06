997b8d12d420f4b0473dbbebe0944d5d5bd17827
Forrest McIntyre <fmcint02@seattlecentral.edu>
2015-07-05 17:33:01 -0700

    fix default order for initial build

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,5 +58,5 @@ module.exports = function(grunt){
       }
    });
 
-   grunt.registerTask("default", ["autoprefixer", "less", "concurrent"]);
+   grunt.registerTask("default", ["less", "autoprefixer", "concurrent"]);
 }
